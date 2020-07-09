b37202201b2f703cbce8556c0436984f1ce86235
krnso <krnso@DESKTOP-V2JJQVP>
2020-07-09 13:52:19 +0900

    2020070902

---
+++ ncapl/src/main/java/com/example/demo/model/Flg.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="t_flg")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Flg {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="value")
+    private String value;
+}
