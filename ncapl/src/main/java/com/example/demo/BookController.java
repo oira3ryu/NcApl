2b3e3cc70619f5eba4808beecc05beaab80557e4
krnso <krnso@DESKTOP-V2JJQVP>
2020-07-20 16:44:44 +0900

    2020072001ドロップダウンに変更し初期意をDBと同期

--- ncapl/src/main/java/com/example/demo/BookController.java
+++ ncapl/src/main/java/com/example/demo/BookController.java
@@ -11,10 +11,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.model.Aggregate;
 import com.example.demo.model.Book;
+import com.example.demo.model.Company;
+import com.example.demo.model.Composition;
+import com.example.demo.model.Field;
+import com.example.demo.model.Flg;
 import com.example.demo.model.Journal;
+import com.example.demo.model.Plant;
+import com.example.demo.model.Slump;
+import com.example.demo.model.Strength;
+import com.example.demo.model.Type;
+import com.example.demo.model.Unit;
+import com.example.demo.service.AggregateService;
 import com.example.demo.service.BookService;
+import com.example.demo.service.CompanyService;
+import com.example.demo.service.CompositionService;
+import com.example.demo.service.FieldService;
+import com.example.demo.service.FlgService;
 import com.example.demo.service.JournalService;
+import com.example.demo.service.PlantService;
+import com.example.demo.service.SlumpService;
+import com.example.demo.service.StrengthService;
+import com.example.demo.service.TypeService;
+import com.example.demo.service.UnitService;
 
 @Controller
 @RequestMapping("/book")
@@ -24,7 +44,38 @@ public class BookController {
 	private BookService bookService;
 
 	@Autowired
+	private PlantService plantService;
+
+	@Autowired
 	private JournalService journalService;
+
+	@Autowired
+	private StrengthService strengthService;
+
+	@Autowired
+	private AggregateService aggregateService;
+
+	@Autowired
+	private CompositionService compositionService;
+
+	@Autowired
+	private CompanyService companyService;
+
+	@Autowired
+	private FieldService fieldService;
+
+	@Autowired
+	private FlgService flgService;
+
+	@Autowired
+	private SlumpService slumpService;
+
+	@Autowired
+	private TypeService typeService;
+
+	@Autowired
+	private UnitService unitService;
+
 
 	@GetMapping
 	  public String bookIndex(Model model) {
@@ -42,8 +93,40 @@ public class BookController {
 	public String edit(@PathVariable Integer id,Model model) {
 		Book book = bookService.findOne(id);
 		model.addAttribute("book",book);
+
+		List<Plant> plants = plantService.findAll();
+		model.addAttribute("plantlist", plants);
+
 		List<Journal> journals = journalService.findAll();
-		model.addAttribute("list", journals);
+		model.addAttribute("journallist", journals);
+
+		List<Strength> strengths = strengthService.findAll();
+		model.addAttribute("strengthlist", strengths);
+
+		List<Aggregate> aggregates = aggregateService.findAll();
+		model.addAttribute("aggregatelist", aggregates);
+
+		List<Composition> compositions = compositionService.findAll();
+		model.addAttribute("compositionlist", compositions);
+
+		List<Company> companys = companyService.findAll();
+		model.addAttribute("companylist", companys);
+
+		List<Field> fields = fieldService.findAll();
+		model.addAttribute("fieldlist", fields);
+
+		List<Flg> flgs = flgService.findAll();
+		model.addAttribute("flglist", flgs);
+
+		List<Slump> slumps = slumpService.findAll();
+		model.addAttribute("slumplist", slumps);
+
+		List<Type> types = typeService.findAll();
+		model.addAttribute("typelist", types);
+
+		List<Unit> units = unitService.findAll();
+		model.addAttribute("unitlist", units);
+
 		return "bookEdit";
 	}
 
