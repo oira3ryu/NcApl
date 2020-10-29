980ee45f0887b1874c7887e22e032ce3ec0c2ee4
yukihito miso <yukihito.miso@gmail.com>
2020-10-29 16:41:50 +0900

    2020102901

--- ncapl/src/main/java/com/example/demo/repository/Product_name_view_jidRepository.java
+++ ncapl/src/main/java/com/example/demo/repository/Product_name_view_jidRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Product_name_view_jid;
 @Repository
-	public interface Product_name_view_jidRepository extends JpaRepository<Product_name_view_jid, Integer> {}
+	public interface Product_name_view_jidRepository extends JpaRepository<Product_name_view_jid, Integer> {
+	@Query("select t from Product_name_view_jid t where jid = :jid")
+	public List<Product_name_view_jid> findGeValue(@Param("jid") String jid);
+}
